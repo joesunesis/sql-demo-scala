42b8295439c7ade7b13ef1bb76f0df2794fe081d
joe <joe@gmail.com>
2021-11-24 15:50:38 +0000

    Database schema and stored procedures for online store

---
+++ online-store-db-test-queries.sql
@@ -0,0 +1,19 @@
+CALL GETOrdersOverMonths(5);
+
+ 
+CALL GetProductsInStock;
+
+
+CALL GetProductsOutOfStock;
+
+ 
+CALL GetCustomersWithMoreThanAnOrder;
+
+
+CALL GetCustomersPurchasedProduct('table');
+
+
+CALL GetCustomersSpenditures('josh');
+
+
+CALL GetProductSales('chair');
